3dc38a397fac4deac10843084f14c33baf8daa07
锋 杨 <yangf85@163.com>
2022-09-20 20:31:52 +0800

    添加项目文件。

---
+++ SqliteTest/Models/School.cs
@@ -0,0 +1,55 @@
+﻿using FreeSql.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteTest.Models
+{
+    public class Grade
+    {
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int Id { get; set; }
+
+        public int Index { get; set; }
+        public List<Team> Teams { get; set; }
+    }
+
+    public class Student
+    {
+        public string Age { get; set; }
+
+        public int GradeId { get; set; }
+
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int TeamId { get; set; }
+    }
+
+    public class Teacher
+    {
+        public string Age { get; set; }
+
+        public int GradeId { get; set; }
+
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int TeamId { get; set; }
+    }
+
+    public class Team
+    {
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public List<Student> Students { get; set; }
+        public int TeacherId { get; set; }
+    }
+}
